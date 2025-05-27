fe04d9cb0cf9c452c3ae51261d6b7ae257e68659
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-05-26 23:59:21 -0300

    ts

--- lernprojekt/myapp/src/lib/Menunav.svelte
+++ lernprojekt/myapp/src/lib/Menunav.svelte
@@ -15,7 +15,7 @@
         {#each   itens as {href,label} }
             <a {href} class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">{label}</a>
         {/each}
-    <!-- <a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6" alt="Logo">Inicio</a> -->
+     <!-- <a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6" alt="Logo">Inicio</a>  -->
        
 
         
